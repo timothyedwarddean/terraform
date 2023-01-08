ea1893d1cb6d0a024d3b39fcf48f71b9bb8f011a
Tim Dean <timothy.edward.dean@gmail.com>
2023-01-08 12:32:47 -0500

    hidden resources

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-# Configure the AWS Provider
+# Configure the AWS provider with secrets.
 provider "aws" {
-  region = "us-east-1"
+  profile = "default_profile"
+  access_key = "@{var.aws_access_key_id}"
+  secret_key = "@{var.aws_secret_access_key}"
+  region = "@{var.aws_region}"
 }
 
 # Create a VPC
 resource "aws_vpc" "tims-vpc"{
     cidr_block = "10.0.0.0/16"
 }
-
