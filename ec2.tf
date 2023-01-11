1a1a55680c8301787cdd8307c7016964afccff0b
Tim Dean <timothy.edward.dean@gmail.com>
2023-01-10 22:42:38 -0500

    updated ec2

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,25 @@
-resource "aws_instance" "phpBB_ec2" {
-    ami = "ami-0627a38def1fdd112"
-    instance_type = t2.micro
+data "aws_ami" "phpbb_ami" {
+    most_recent = true
+    filter {
+        name = "phpBB packaged by Bitnami"
+    }
+}
+
+resource "aws_network_interface" "phpbb_server" {
+  subnet_id   = aws_subnet.public_subnet_1.id
+  private_ips = ["10.0.1.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "phpbb_server" {
+  ami           = data.aws_ami.phpbb_ami
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.phpbb_server.id
+    device_index         = 0
+  }
 }
